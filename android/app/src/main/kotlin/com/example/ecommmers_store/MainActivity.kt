590feaa4e544b0cb53c53a0a4b6cb662b1667620
ahmedalnj <ahmed.alnjjar40@gmail.com>
2025-12-24 14:03:47 +0200

    initial

---
+++ android/app/src/main/kotlin/com/example/ecommmers_store/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ecommmers_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ecommmers_store/view/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Examples App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
